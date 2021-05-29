dd57852186216b422c5e932451ab25a1fa4631f6
Stuart Lynn <stuart.lynn@gmail.com>
2021-05-29 15:55:39 -0400

    Setting up basic routing and app structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,17 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import {BrowserRouter as Router} from 'react-router-dom'
+import {Switch,Route} from 'react-router'
+import {HomePage} from 'Pages/HomePage/HomePage'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path='/' component={HomePage} exact/>
+        </Switch>
+      </Router>
     </div>
   );
 }
