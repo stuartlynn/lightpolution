c1fd3fba696bb4f9a69ee498a26443317843a04c
Stuart Lynn <stuart.lynn@gmail.com>
2021-06-01 22:06:33 -0400

    Fixing build script

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 cargo build --release
 (cd frontend &&  yarn && yarn build)
-mv frontned/build/* ./public
+mv frontend/build ./public
 cargo install diesel_cli --no-default-features --features postgres
 diesel database setup
