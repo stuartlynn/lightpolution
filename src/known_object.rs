6bd10943ab528dc7571c8928109d8535ba7add26
Stuart Lynn <stuart.lynn@gmail.com>
2021-07-03 17:25:58 -0400

    model for known object

--- src/known_object.rs
+++ src/known_object.rs
@@ -2,10 +2,30 @@ use serde::{Serialize,Deserialize}
 
 #[derive(Queryable, Serialize,Deserialize)]
 pub struct KnownObject{
-    pub id: u32,
+    pub id: i32,
     pub name: String,
     pub location: Option<Point>,
     pub known_object_type: Option<String>,
 }
 
-impl from
+impl From<PgRow> for KnownObject{
+    fn from(row:PgRow) ->Self{
+        let location: wkb::Decode<geo_types::Geometry<f64>> = row.get("location");
+        let location = if let Some(geo_types::Geometry::Point(point)) = location.geometry{
+            Some(point)
+        }
+        else{
+            None
+        };
+
+        println!("location is {:?}", location);
+
+        Self{
+            id: row.get("id"),
+            name: row.get("name"),
+            location,
+            extent,
+            known_object_type: row.get("known_object_type"),
+        }
+    }
+}
